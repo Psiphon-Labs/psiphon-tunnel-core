c57613b23d0996eafbb11100a03461dd95cfff5a
Rod Hynes <rod-hynes@users.noreply.github.com>
2014-11-03 11:11:09 -0500

    Fix compile errors on Windows

--- psiphon/LookupIP_nobind.go
+++ psiphon/LookupIP_nobind.go
@@ -28,7 +28,7 @@ import (
 // LookupIP resolves a hostname. When BindToDevice is not required, it
 // simply uses net.LookuIP.
 func LookupIP(host string, config *DialConfig) (addrs []net.IP, err error) {
-	if config.BindToDeviceServiceAddr != "" {
+	if config.BindToDeviceServiceAddress != "" {
 		Fatal("LookupIP with bind not supported on this platform")
 	}
 	return net.LookupIP(host)
