04e273d121014027f099635b14db81fbefb60760
Adam Pritchard <pritchard.adam@gmail.com>
2014-12-21 19:56:59 -0500

    Added 'all' and 'verbose' flags to build command

--- AndroidLibrary/make.bash
+++ AndroidLibrary/make.bash
@@ -10,7 +10,7 @@ fi
 ANDROID_APP=$PWD
 
 CGO_ENABLED=1 GOOS=android GOARCH=arm GOARM=7 \
-  go build -ldflags="-shared" -o libgojni.so ./libpsi
+  go build -a -v -ldflags="-shared" -o libgojni.so ./libpsi
 
 mkdir -p libs/armeabi-v7a
 mv -f libgojni.so libs/armeabi-v7a/libgojni.so
