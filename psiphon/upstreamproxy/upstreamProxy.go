d0182ba359f2589749638c55e5b503e2e69d396a
Eugene Fryntov <e.fryntov@psiphon.ca>
2015-07-17 18:04:46 -0400

    Completing files commit for upstreamproxy.Error type. Do more robust error check on the handshake return error now that ErrPersistEOF check moved into the handshake itself

--- psiphon/upstreamproxy/upstreamProxy.go
+++ psiphon/upstreamproxy/upstreamProxy.go
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2015, Psiphon Inc.
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package upstreamproxy
 
 import (
@@ -8,6 +27,19 @@ import (
 )
 
 type DialFunc func(string, string) (net.Conn, error)
+
+type Error struct {
+	error
+}
+
+func proxyError(err error) error {
+	//Avoid multiple upstream.Error wrapping
+	if _, ok := err.(Error); ok {
+		return err
+	}
+	format := fmt.Sprintf("upstreamproxy error: %v", err)
+	return &Error{error: fmt.Errorf(format, err)}
+}
 
 type UpstreamProxyConfig struct {
 	ForwardDialFunc DialFunc
@@ -27,14 +59,14 @@ func NewProxyDialFunc(config *UpstreamProxyConfig) DialFunc {
 	proxyURI, err := url.Parse(config.ProxyURIString)
 	if err != nil {
 		return func(network, addr string) (net.Conn, error) {
-			return nil, fmt.Errorf("Upstream proxy URI parsing error: %v", err)
+			return nil, proxyError(fmt.Errorf("proxyURI url.Parse: %v", err))
 		}
 	}
 
 	dialer, err := proxy.FromURL(proxyURI, config)
 	if err != nil {
 		return func(network, addr string) (net.Conn, error) {
-			return nil, err
+			return nil, proxyError(fmt.Errorf("proxy.FromURL: %v", err))
 		}
 	}
 	return dialer.Dial
