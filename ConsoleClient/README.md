3f3a7d044fbb714d3c018502e065e4a0a65280a8
Michael Goldberger <mike.goldberger@gmail.com>
2015-09-25 14:48:55 -0400

    Updated Dockerfile and makefile to use go 1.5 and cross compile binaries for windows, linux, and osx; updated ConsoleClient README to use docker instructions; Added subheadings (for anchors) to main README

--- ConsoleClient/README.md
+++ ConsoleClient/README.md
@@ -1,6 +1,43 @@
-Psiphon Console Client README
-================================================================================
+##Psiphon Console Client README
 
-### Setup
+###Building with Docker
 
-See: https://github.com/Psiphon-Labs/psiphon-tunnel-core#setup
+Note that you may need to use `sudo docker` below, depending on your OS.
+
+#####Create the build image:
+  1. Run the command: `docker build --no-cache=true -t psiclient .` (this may take some time to complete)
+  2. Once completed, verify that you see an image named `psiclient` when running: `docker images`
+
+#####Run the build:
+
+  ```bash
+  cd .. && \
+    docker run \
+    --rm \
+    -v $(pwd):/go/src/github.com/Psiphon-Labs/psiphon-tunnel-core \
+    psiclient \
+    /bin/bash -c 'cd /go/src/github.com/Psiphon-Labs/psiphon-tunnel-core/ConsoleClient && ./make.bash' \
+  && cd -
+  ```
+
+When that command completes, the compiled binaries will be located in the `bin` directory under the current directory. The structure will be:
+  ```
+  bin
+  ├── darwin
+  │   └── psiphon-native-messaging-host-x86_64
+  ├── linux
+  │   └── psiphon-native-messaging-host-i686
+  │   └── psiphon-native-messaging-host-x86_64
+  └── windows
+      └── psiphon-native-messaging-host-i686.exe
+      └── psiphon-native-messaging-host-x86_64.exe
+
+  ```
+
+### Building without Docker
+
+See the [main README build section](../README.md#build)
+
+### Creating a configuration file
+
+See the [main README configuration section](../README.md#configure)
--- ConsoleClient/make.bash
+++ ConsoleClient/make.bash
@@ -1,49 +1,70 @@
 #!/usr/bin/env bash
 
 set -e
-set -exv # verbose output for testing
 
 if [ ! -f make.bash ]; then
-  echo 'make.bash must be run from $GOPATH/src/github.com/Psiphon-Labs/psiphon-tunnel-core/ConsoleClient'
+  echo "make.bash must be run from $GOPATH/src/github.com/Psiphon-Labs/psiphon-tunnel-core/ConsoleClient"
   exit 1
 fi
-
-CGO_ENABLED=1
-
-# Make sure we have our dependencies
-echo -e "go-getting dependencies...\n"
-go get -d -v ./...
 
 EXE_BASENAME="psiphon-tunnel-core"
 BUILDINFOFILE="${EXE_BASENAME}_buildinfo.txt"
 BUILDDATE=$(date --iso-8601=seconds)
 BUILDREPO=$(git config --get remote.origin.url)
-BUILDREV=$(git rev-parse HEAD)
+BUILDREV=$(git rev-parse --short HEAD)
+
 LDFLAGS="\
 -X github.com/Psiphon-Labs/psiphon-tunnel-core/psiphon.buildDate $BUILDDATE \
 -X github.com/Psiphon-Labs/psiphon-tunnel-core/psiphon.buildRepo $BUILDREPO \
 -X github.com/Psiphon-Labs/psiphon-tunnel-core/psiphon.buildRev $BUILDREV \
 "
 echo -e "${BUILDDATE}\n${BUILDREPO}\n${BUILDREV}\n" > $BUILDINFOFILE
-echo -e "LDFLAGS=$LDFLAGS\n"
 
-echo -e "\nBuilding windows-386..."
-CC=/usr/bin/i686-w64-mingw32-gcc \
-  gox -verbose -ldflags "$LDFLAGS" -osarch windows/386 -output windows_386_${EXE_BASENAME}
-# We are finding that UPXing the full Windows Psiphon client produces better results
-# if psiphon-tunnel-core.exe is not already UPX'd.
-#upx --best windows_386_${EXE_BASENAME}.exe
+echo "Variables for ldflags:"
+echo " Build date: ${BUILDDATE}"
+echo " Build repo: ${BUILDREPO}"
+echo " Build revision: ${BUILDREV}"
+echo ""
 
-echo -e "\nBuilding windows-amd64..."
-CC=/usr/bin/x86_64-w64-mingw32-gcc \
-  gox -verbose -ldflags "$LDFLAGS" -osarch windows/amd64 -output windows_amd64_${EXE_BASENAME}
-upx --best windows_amd64_${EXE_BASENAME}.exe
+echo "Getting project dependencies (via go get)"
+GOOS=linux go get -d -v ./...
+GOOS=windows go get -d -v ./...
+GOOS=darwin go get -d -v ./...
 
-echo -e "\nBuilding linux-amd64..."
-gox -verbose -ldflags "$LDFLAGS" -osarch linux/amd64 -output linux_amd64_${EXE_BASENAME}
-upx --best linux_amd64_${EXE_BASENAME}
+if [ ! -d bin ]; then
+  mkdir bin
+fi
 
-echo -e "\nBuilding linux-386..."
-CFLAGS=-m32 \
-  gox -verbose -ldflags "$LDFLAGS" -osarch linux/386 -output linux_386_${EXE_BASENAME}
-upx --best linux_386_${EXE_BASENAME}
+# Windows requires CGO due to sqlite. OpenSSL will likely eventually require CGO everywhere
+echo "CGO Enabled"
+export CGO_ENABLED=1
+
+echo "Building windows-i686..."
+CC=/usr/bin/i686-w64-mingw32-gcc gox -verbose -ldflags "$LDFLAGS" -osarch windows/386 -output bin/windows/${EXE_BASENAME}-i686
+# We are finding that UPXing the full Windows Psiphon client produces better results if psiphon-tunnel-core.exe is not already UPX'd.
+echo "..No UPX for this build"
+
+echo "Building windows-x86_64..."
+CC=/usr/bin/x86_64-w64-mingw32-gcc gox -verbose -ldflags "$LDFLAGS" -osarch windows/amd64 -output bin/windows/${EXE_BASENAME}-x86_64
+# We are finding that UPXing the full Windows Psiphon client produces better results if psiphon-tunnel-core.exe is not already UPX'd.
+echo "..No UPX for this build"
+
+echo "CGO Enabled unset (Preferred unless needed)"
+unset CGO_ENABLED
+
+echo "Building linux-i686..."
+CFLAGS=-m32 gox -verbose -ldflags "$LDFLAGS" -osarch linux/386 -output bin/linux/${EXE_BASENAME}-i686
+echo "..UPX packaging output"
+goupx --best bin/linux/${EXE_BASENAME}-i686
+
+echo "Building linux-x86_64..."
+gox -verbose -ldflags "$LDFLAGS" -osarch linux/amd64 -output bin/linux/${EXE_BASENAME}-x86_64
+echo "..UPX packaging output"
+goupx --best bin/linux/${EXE_BASENAME}-x86_64
+
+echo "Building darwin-x86_64..."
+gox -verbose -ldflags "$LDFLAGS" -osarch darwin/amd64 -output bin/darwin/${EXE_BASENAME}-x86_64
+# Darwin binaries don't seem to be UPXable when built this way
+echo "..No UPX for this build"
+
+echo "Done"
