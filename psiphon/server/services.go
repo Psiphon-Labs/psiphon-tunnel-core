39ca4f64940befe11333ef23342925f4d11f377c
Michael Goldberger <mike.goldberger@gmail.com>
2016-08-16 12:00:08 -0400

    moved load log fields to sub-objects where they were previously '.' separated

--- psiphon/server/services.go
+++ psiphon/server/services.go
@@ -153,22 +153,27 @@ func logServerLoad(server *TunnelServer) {
 	var memStats runtime.MemStats
 	runtime.ReadMemStats(&memStats)
 	fields := LogFields{
-		"BuildRev":               common.GetBuildInfo().BuildRev,
-		"NumGoroutine":           runtime.NumGoroutine(),
-		"MemStats.Alloc":         memStats.Alloc,
-		"MemStats.TotalAlloc":    memStats.TotalAlloc,
-		"MemStats.Sys":           memStats.Sys,
-		"MemStats.PauseTotalNs":  memStats.PauseTotalNs,
-		"MemStats.PauseNs":       memStats.PauseNs,
-		"MemStats.NumGC":         memStats.NumGC,
-		"MemStats.GCCPUFraction": memStats.GCCPUFraction,
+		"BuildRev":     common.GetBuildInfo().BuildRev,
+		"NumGoroutine": runtime.NumGoroutine(),
+		"MemStats": map[string]interface{}{
+			"Alloc":         memStats.Alloc,
+			"TotalAlloc":    memStats.TotalAlloc,
+			"Sys":           memStats.Sys,
+			"PauseTotalNs":  memStats.PauseTotalNs,
+			"PauseNs":       memStats.PauseNs,
+			"NumGC":         memStats.NumGC,
+			"GCCPUFraction": memStats.GCCPUFraction,
+		},
 	}
 
 	// tunnel server stats
 	for tunnelProtocol, stats := range server.GetLoadStats() {
+		tempMap := make(map[string]interface{})
 		for stat, value := range stats {
-			fields[tunnelProtocol+"."+stat] = value
+			tempMap[stat] = value
 		}
+
+		fields[tunnelProtocol] = tempMap
 	}
 
 	log.WithContextFields(fields).Info("load")
