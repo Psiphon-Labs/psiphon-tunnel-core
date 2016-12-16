1ab9d4ab4b3d0cf0423232eee219d78ef4247dfc
Rod Hynes <rod-hynes@users.noreply.github.com>
2016-12-16 12:12:17 -0500

    Use = delimiter for tags argument to gomobile

--- MobileLibrary/Android/make.bash
+++ MobileLibrary/Android/make.bash
@@ -60,7 +60,7 @@ echo " Gomobile version: ${GOMOBILEVERSION}"
 echo " Dependencies: ${DEPENDENCIES}"
 echo ""
 
-gomobile bind -v -target=android/arm -tags "${BUILD_TAGS}" -ldflags="$LDFLAGS" github.com/Psiphon-Labs/psiphon-tunnel-core/MobileLibrary/psi
+gomobile bind -v -target=android/arm -tags="${BUILD_TAGS}" -ldflags="$LDFLAGS" github.com/Psiphon-Labs/psiphon-tunnel-core/MobileLibrary/psi
 if [ $? != 0 ]; then
   echo "..'gomobile bind' failed, exiting"
   exit $?
