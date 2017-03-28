0ba4fff071e91daac763614de7257c6d3419ec3c
Rod Hynes <rod-hynes@users.noreply.github.com>
2017-03-28 07:18:39 -0400

    Fix: syntax error

--- MobileLibrary/Android/PsiphonTunnel/PsiphonTunnel.java
+++ MobileLibrary/Android/PsiphonTunnel/PsiphonTunnel.java
@@ -498,7 +498,7 @@ public class PsiphonTunnel extends Psi.PsiphonProvider.Stub {
                 JSONObject data = notice.getJSONObject("data");
                 mHostService.onClientVerificationRequired(data.getString("nonce"), data.getInt("ttlSeconds"), data.getBoolean("resetCache"));
             } else if (noticeType.equals("ActiveTunnel")) {
-                if (isVPNMode()) {
+                if (isVpnMode()) {
                     if (notice.getJSONObject("data").getBoolean("isTCS")) {
                       disableUdpGwKeepalive();
                     } else {
