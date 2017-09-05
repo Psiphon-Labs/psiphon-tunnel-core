81a334b6c6ce855308d4480bb46db00b06531a64
Rod Hynes <rod-hynes@users.noreply.github.com>
2017-09-05 14:30:07 -0400

    Fix: only apply SO_NOSIGPIPE on darwin

--- psiphon/TCPConn_bind.go
+++ psiphon/TCPConn_bind.go
@@ -124,7 +124,7 @@ func tcpDial(addr string, config *DialConfig) (net.Conn, error) {
 			continue
 		}
 
-		syscall.SetsockoptInt(socketFd, syscall.SOL_SOCKET, syscall.SO_NOSIGPIPE, 1)
+		tcpDialSetAdditionalSocketOptions(socketFd)
 
 		if config.DeviceBinder != nil {
 			// WARNING: this potentially violates the direction to not call into
