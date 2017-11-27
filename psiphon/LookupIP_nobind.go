Psiphon-Labs/psiphon-tunnel-core

commit 48611aff343c3dd4f98c5e0d5d43c87af990afaf

Replace pending conns with Context
- The pending conns structure was used to track dials in-progress in order to
  interrupt operations blocking on network events. This mechanism was error
  prone and difficult to manage.

- Contexts are now used for all operational phases and for all network dials.
  When the context for a phase is done (times out or is canceled), all related
  dials will be interrupted.

- This fixes related unbounded memory growth: untunneled dials such as those
  for remote server list fetches would leave conns in a pending conns structure
  and never remove them until the Controller stopped. Similarly, MeekConn
  redials would retain underlyign conns in a pending conns structure and never
  remove them until the MeekConn itself was closed.

- Custom TCP dials will now no longer leave goroutines running when dials are
  interrupted. This ensures that resource scheduling -- not running too many
  dials simultaneously in low memory conditions -- works as intended.

- Where context.Context is not supported -- tls, ssh, proxy etc. -- operations
  that block on network operations are run in a temporary goroutine and
  interrupted by closing underlying network connections.

- Timeout is removed from DialConfig and CustomTLSConfig and parent contexts
  are expected to be configured with deadlines as appropriate.

- Several bugs with pending conns operations related to copying or replacing
  the PendingConns in DialConfig are now gone.

- The untunneled final status request is removed. As this required a capability
  that compromised circumvention, it was rarely used. It simplified the code
  to remove this request rather than convert it to use contexts.

- The experimental RegisterPluginProtocol, which used pending conns, is removed
  and will be replaced with a mechanism using contexts when required.

- DialTLS is used instead of hacking the URL scheme in HTTPS requests made
  using "net/http" and custom TLS dialers.

psiphon/LookupIP_nobind.go
@@ -22,6 +22,7 @@
 package psiphon
 
 import (
+	"context"
 	"errors"
 	"net"
 
@@ -30,9 +31,21 @@ import (
 
 // LookupIP resolves a hostname. When BindToDevice is not required, it
 // simply uses net.LookupIP.
-func LookupIP(host string, config *DialConfig) (addrs []net.IP, err error) {
+func LookupIP(ctx context.Context, host string, _ *DialConfig) ([]net.IP, error) {
+
 	if config.DeviceBinder != nil {
 		return nil, common.ContextError(errors.New("LookupIP with DeviceBinder not supported on this platform"))
 	}
-	return net.LookupIP(host)
+
+	addrs, err := net.DefaultResolver.LookupIPAddr(ctx, host)
+	if err != nil {
+		return nil, common.ContextError(err)
+	}
+
+	ips := make([]netIP, len(addrs))
+	for i, addr := range addrs {
+		ips[i] = addr.IP
+	}
+
+	return ips, nil
 }

psiphon/TCPConn_nobind.go
@@ -22,18 +22,27 @@
 package psiphon
 
 import (
+	"context"
 	"errors"
 	"net"
 
 	"github.com/Psiphon-Labs/psiphon-tunnel-core/psiphon/common"
 )
 
-// tcpDial is the platform-specific part of interruptibleTCPDial
-func tcpDial(addr string, config *DialConfig) (net.Conn, error) {
+// tcpDial is the platform-specific part of DialTCP
+func tcpDial(ctx context.Context, addr string, config *DialConfig) (net.Conn, error) {
 
 	if config.DeviceBinder != nil {
 		return nil, common.ContextError(errors.New("psiphon.interruptibleTCPDial with DeviceBinder not supported"))
 	}
 
-	return net.DialTimeout("tcp", addr, config.ConnectTimeout)
+	dialer := net.Dialer{}
+
+	conn, err := dialer.DialContext(ctx, "tcp", addr, config.ConnectTimeout)
+
+	if err != nil {
+		return nil, common.ContextError(err)
+	}
+
+	return &TCPConn{Conn: conn}, nil
 }

psiphon/LookupIP.go
@@ -22,12 +22,12 @@
 package psiphon
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"net"
 	"os"
 	"syscall"
-	"time"
 
 	"github.com/Psiphon-Labs/psiphon-tunnel-core/psiphon/common"
 )
@@ -37,38 +37,53 @@ import (
 // When BindToDevice is required, LookupIP explicitly creates a UDP
 // socket, binds it to the device, and makes an explicit DNS request
 // to the specified DNS resolver.
-func LookupIP(host string, config *DialConfig) (addrs []net.IP, err error) {
+func LookupIP(ctx context.Context, host string, config *DialConfig) ([]net.IP, error) {
 
-	// When the input host is an IP address, echo it back
-	ipAddr := net.ParseIP(host)
-	if ipAddr != nil {
-		return []net.IP{ipAddr}, nil
+	ip := net.ParseIP(host)
+	if ip != nil {
+		return []net.IP{ip}, nil
 	}
 
 	if config.DeviceBinder != nil {
-		addrs, err = bindLookupIP(host, config.DnsServerGetter.GetPrimaryDnsServer(), config)
+
+		dnsServer := config.DnsServerGetter.GetPrimaryDnsServer()
+
+		ips, err := bindLookupIP(ctx, host, dnsServer, config)
 		if err == nil {
-			if len(addrs) == 0 {
+			if len(ips) == 0 {
 				err = errors.New("empty address list")
 			} else {
-				return addrs, err
+				return ips, err
 			}
 		}
-		NoticeAlert("retry resolve host %s: %s", host, err)
-		dnsServer := config.DnsServerGetter.GetSecondaryDnsServer()
+
+		dnsServer = config.DnsServerGetter.GetSecondaryDnsServer()
 		if dnsServer == "" {
-			return addrs, err
+			return ips, err
 		}
-		return bindLookupIP(host, dnsServer, config)
+
+		NoticeAlert("retry resolve host %s: %s", host, err)
+
+		return bindLookupIP(ctx, host, dnsServer, config)
 	}
-	return net.LookupIP(host)
+
+	addrs, err := net.DefaultResolver.LookupIPAddr(ctx, host)
+	if err != nil {
+		return nil, common.ContextError(err)
+	}
+
+	ips := make([]net.IP, len(addrs))
+	for i, addr := range addrs {
+		ips[i] = addr.IP
+	}
+
+	return ips, nil
 }
 
 // bindLookupIP implements the BindToDevice LookupIP case.
 // To implement socket device binding, the lower-level syscall APIs are used.
-// The sequence of syscalls in this implementation are taken from:
-// https://code.google.com/p/go/issues/detail?id=6966
-func bindLookupIP(host, dnsServer string, config *DialConfig) (addrs []net.IP, err error) {
+func bindLookupIP(
+	ctx context.Context, host, dnsServer string, config *DialConfig) ([]net.IP, error) {
 
 	// config.DnsServerGetter.GetDnsServers() must return IP addresses
 	ipAddr := net.ParseIP(dnsServer)
@@ -130,6 +145,9 @@ func bindLookupIP(host, dnsServer string, config *DialConfig) (addrs []net.IP, e
 	}
 
 	// Convert the syscall socket to a net.Conn, for use in the dns package
+	// This code block is from:
+	// https://github.com/golang/go/issues/6966
+
 	file := os.NewFile(uintptr(socketFd), "")
 	netConn, err := net.FileConn(file) // net.FileConn() dups socketFd
 	file.Close()                       // file.Close() closes socketFd
@@ -137,17 +155,33 @@ func bindLookupIP(host, dnsServer string, config *DialConfig) (addrs []net.IP, e
 		return nil, common.ContextError(err)
 	}
 
-	// Set DNS query timeouts, using the ConnectTimeout from the overall Dial
-	if config.ConnectTimeout != 0 {
-		netConn.SetReadDeadline(time.Now().Add(config.ConnectTimeout))
-		netConn.SetWriteDeadline(time.Now().Add(config.ConnectTimeout))
+	type resolveIPResult struct {
+		ips []net.IP
+		err error
 	}
 
-	addrs, _, err = ResolveIP(host, netConn)
-	netConn.Close()
-	if err != nil {
+	resultChannel := make(chan resolveIPResult)
+
+	go func() {
+		ips, _, err := ResolveIP(host, netConn)
+		netConn.Close()
+		resultChannel <- resolveIPResult{ips: ips, err: err}
+	}()
+
+	var result resolveIPResult
+
+	select {
+	case result = <-resultChannel:
+	case <-ctx.Done():
+		result.err = ctx.Err()
+		// Interrupt the goroutine
+		netConn.Close()
+		<-resultChannel
+	}
+
+	if result.err != nil {
 		return nil, common.ContextError(err)
 	}
 
-	return addrs, nil
+	return result.ips, nil
 }

psiphon/TCPConn.go
@@ -20,137 +20,92 @@
 package psiphon
 
 import (
+	"context"
 	"errors"
+	"fmt"
 	"net"
-	"sync"
+	"sync/atomic"
 
 	"github.com/Psiphon-Labs/psiphon-tunnel-core/psiphon/common"
 	"github.com/Psiphon-Labs/psiphon-tunnel-core/psiphon/upstreamproxy"
 )
 
-// TCPConn is a customized TCP connection that:
-// - can be interrupted while dialing;
-// - implements a connect timeout;
-// - uses an upstream proxy when specified, and includes
-//   upstream proxy dialing in the connect timeout;
-// - can be bound to a specific system device (for Android VpnService
-//   routing compatibility, for example);
+// TCPConn is a customized TCP connection that supports the Closer interface
+// and which may be created using options in DialConfig, including
+// UpstreamProxyUrl, DeviceBinder, IPv6Synthesizer, and ResolvedIPCallback.
+// DeviceBinder is implemented using SO_BINDTODEVICE/IP_BOUND_IF, which
+// requires syscall-level socket code.
 type TCPConn struct {
 	net.Conn
-	mutex      sync.Mutex
-	isClosed   bool
-	dialResult chan error
+	isClosed int32
 }
 
 // NewTCPDialer creates a TCPDialer.
+//
+// Note: do not set an UpstreamProxyUrl in the config when using NewTCPDialer
+// as a custom dialer for NewProxyAuthTransport (or http.Transport with a
+// ProxyUrl), as that would result in double proxy chaining.
 func NewTCPDialer(config *DialConfig) Dialer {
-	return makeTCPDialer(config)
+	return func(ctx context.Context, network, addr string) (net.Conn, error) {
+		if network != "tcp" {
+			return nil, common.ContextError(fmt.Errorf("%s unsupported", network))
+		}
+		return DialTCP(ctx, addr, config)
+	}
 }
 
 // DialTCP creates a new, connected TCPConn.
-func DialTCP(addr string, config *DialConfig) (conn net.Conn, err error) {
-	return makeTCPDialer(config)("tcp", addr)
-}
+func DialTCP(
+	ctx context.Context, addr string, config *DialConfig) (net.Conn, error) {
 
-// makeTCPDialer creates a custom dialer which creates TCPConn.
-func makeTCPDialer(config *DialConfig) func(network, addr string) (net.Conn, error) {
-	return func(network, addr string) (net.Conn, error) {
-		if network != "tcp" {
-			return nil, errors.New("unsupported network type in TCPConn dialer")
-		}
-		conn, err := interruptibleTCPDial(addr, config)
-		if err != nil {
-			return nil, common.ContextError(err)
-		}
-		// Note: when an upstream proxy is used, we don't know what IP address
-		// was resolved, by the proxy, for that destination.
-		if config.ResolvedIPCallback != nil && config.UpstreamProxyUrl == "" {
-			ipAddress := common.IPAddressFromAddr(conn.RemoteAddr())
-			if ipAddress != "" {
-				config.ResolvedIPCallback(ipAddress)
-			}
-		}
-		return conn, nil
-	}
-}
+	var conn net.Conn
+	var err error
 
-// interruptibleTCPDial establishes a TCP network connection. A conn is added
-// to config.PendingConns before blocking on network I/O, which enables interruption.
-// The caller is responsible for removing an established conn from PendingConns.
-// An upstream proxy is used when specified.
-//
-// Note: do not to set a UpstreamProxyUrl in the config when using
-// NewTCPDialer as a custom dialer for NewProxyAuthTransport (or http.Transport
-// with a ProxyUrl), as that would result in double proxy chaining.
-//
-// Note: interruption does not actually cancel a connection in progress; it
-// stops waiting for the goroutine blocking on connect()/Dial.
-func interruptibleTCPDial(addr string, config *DialConfig) (*TCPConn, error) {
-
-	// Buffers the first result; senders should discard results when
-	// sending would block, as that means the first result is already set.
-	conn := &TCPConn{dialResult: make(chan error, 1)}
-
-	// Enable interruption
-	if config.PendingConns != nil && !config.PendingConns.Add(conn) {
-		return nil, common.ContextError(errors.New("pending connections already closed"))
+	if config.UpstreamProxyUrl != "" {
+		conn, err = proxiedTcpDial(ctx, addr, config)
+	} else {
+		conn, err = tcpDial(ctx, addr, config)
 	}
 
-	// Call the blocking Connect() in a goroutine. ConnectTimeout is handled
-	// in the platform-specific tcpDial helper function.
-	// Note: since this goroutine may be left running after an interrupt, don't
-	// call Notice() or perform other actions unexpected after a Controller stops.
-	// The lifetime of the goroutine may depend on the host OS TCP connect timeout
-	// when tcpDial, among other things, when makes a blocking syscall.Connect()
-	// call.
-	go func() {
-		var netConn net.Conn
-		var err error
-		if config.UpstreamProxyUrl != "" {
-			netConn, err = proxiedTcpDial(addr, config)
-		} else {
-			netConn, err = tcpDial(addr, config)
-		}
-
-		// Mutex is necessary for referencing conn.isClosed and conn.Conn as
-		// TCPConn.Close may be called while this goroutine is running.
-		conn.mutex.Lock()
-
-		// If already interrupted, cleanup the net.Conn resource and discard.
-		if conn.isClosed && netConn != nil {
-			netConn.Close()
-			conn.mutex.Unlock()
-			return
-		}
-
-		conn.Conn = netConn
-		conn.mutex.Unlock()
-
-		select {
-		case conn.dialResult <- err:
-		default:
-		}
-	}()
-
-	// Wait until Dial completes (or times out) or until interrupt
-	err := <-conn.dialResult
 	if err != nil {
-		if config.PendingConns != nil {
-			config.PendingConns.Remove(conn)
-		}
 		return nil, common.ContextError(err)
 	}
 
-	// TODO: now allow conn.dialResult to be garbage collected?
-
+	// Note: when an upstream proxy is used, we don't know what IP address
+	// was resolved, by the proxy, for that destination.
+	if config.ResolvedIPCallback != nil && config.UpstreamProxyUrl == "" {
+		ipAddress := common.IPAddressFromAddr(conn.RemoteAddr())
+		if ipAddress != "" {
+			config.ResolvedIPCallback(ipAddress)
+		}
+	}
 	return conn, nil
 }
 
 // proxiedTcpDial wraps a tcpDial call in an upstreamproxy dial.
 func proxiedTcpDial(
-	addr string, config *DialConfig) (net.Conn, error) {
+	ctx context.Context, addr string, config *DialConfig) (net.Conn, error) {
+
+	var interruptConns common.Conns
+
+	// Note: using interruptConns to interrupt a proxy dial assumes
+	// that the underlying proxy code will immediately exit with an
+	// error when all underlying conns unexpectedly close; e.g.,
+	// the proxy handshake won't keep retrying to dial new conns.
+
 	dialer := func(network, addr string) (net.Conn, error) {
-		return tcpDial(addr, config)
+		conn, err := tcpDial(ctx, addr, config)
+		if conn != nil {
+			if !interruptConns.Add(conn) {
+				err = errors.New("already interrupted")
+				conn.Close()
+				conn = nil
+			}
+		}
+		if err != nil {
+			return nil, common.ContextError(err)
+		}
+		return conn, nil
 	}
 
 	upstreamDialer := upstreamproxy.NewProxyDialFunc(
@@ -159,56 +114,70 @@ func proxiedTcpDial(
 			ProxyURIString:  config.UpstreamProxyUrl,
 			CustomHeaders:   config.CustomHeaders,
 		})
-	netConn, err := upstreamDialer("tcp", addr)
-	if _, ok := err.(*upstreamproxy.Error); ok {
-		NoticeUpstreamProxyError(err)
+
+	type upstreamDialResult struct {
+		conn net.Conn
+		err  error
 	}
-	return netConn, err
+
+	resultChannel := make(chan upstreamDialResult)
+
+	go func() {
+		conn, err := upstreamDialer("tcp", addr)
+		if _, ok := err.(*upstreamproxy.Error); ok {
+			NoticeUpstreamProxyError(err)
+		}
+		resultChannel <- upstreamDialResult{
+			conn: conn,
+			err:  err,
+		}
+	}()
+
+	var result upstreamDialResult
+
+	select {
+	case result = <-resultChannel:
+	case <-ctx.Done():
+		result.err = ctx.Err()
+		// Interrupt the goroutine
+		interruptConns.CloseAll()
+		<-resultChannel
+	}
+
+	if result.err != nil {
+		return nil, common.ContextError(result.err)
+	}
+
+	return result.conn, nil
 }
 
 // Close terminates a connected TCPConn or interrupts a dialing TCPConn.
 func (conn *TCPConn) Close() (err error) {
-	conn.mutex.Lock()
-	defer conn.mutex.Unlock()
 
-	if conn.isClosed {
-		return
-	}
-	conn.isClosed = true
-
-	if conn.Conn != nil {
-		err = conn.Conn.Close()
+	if !atomic.CompareAndSwapInt32(&conn.isClosed, 0, 1) {
+		return nil
 	}
 
-	select {
-	case conn.dialResult <- errors.New("dial interrupted"):
-	default:
-	}
-
-	return err
+	return conn.Conn.Close()
 }
 
 // IsClosed implements the Closer iterface. The return value
 // indicates whether the TCPConn has been closed.
 func (conn *TCPConn) IsClosed() bool {
-	conn.mutex.Lock()
-	defer conn.mutex.Unlock()
-	return conn.isClosed
+	return atomic.LoadInt32(&conn.isClosed) == 1
 }
 
 // CloseWrite calls net.TCPConn.CloseWrite when the underlying
 // conn is a *net.TCPConn.
 func (conn *TCPConn) CloseWrite() (err error) {
-	conn.mutex.Lock()
-	defer conn.mutex.Unlock()
 
-	if conn.isClosed {
-		return errors.New("already closed")
+	if conn.IsClosed() {
+		return common.ContextError(errors.New("already closed"))
 	}
 
 	tcpConn, ok := conn.Conn.(*net.TCPConn)
 	if !ok {
-		return errors.New("conn is not a *net.TCPConn")
+		return common.ContextError(errors.New("conn is not a *net.TCPConn"))
 	}
 
 	return tcpConn.CloseWrite()

psiphon/TCPConn_bind.go
@@ -22,6 +22,7 @@
 package psiphon
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"math/rand"
@@ -30,18 +31,17 @@ import (
 	"strconv"
 	"syscall"
 
-	"github.com/Psiphon-Inc/goarista/monotime"
 	"github.com/Psiphon-Inc/goselect"
 	"github.com/Psiphon-Labs/psiphon-tunnel-core/psiphon/common"
 )
 
-// tcpDial is the platform-specific part of interruptibleTCPDial
+// tcpDial is the platform-specific part of DialTCP
 //
 // To implement socket device binding, the lower-level syscall APIs are used.
 // The sequence of syscalls in this implementation are taken from:
 // https://github.com/golang/go/issues/6966
 // (originally: https://code.google.com/p/go/issues/detail?id=6966)
-func tcpDial(addr string, config *DialConfig) (net.Conn, error) {
+func tcpDial(ctx context.Context, addr string, config *DialConfig) (net.Conn, error) {
 
 	// Get the remote IP and port, resolving a domain name if necessary
 	host, strPort, err := net.SplitHostPort(addr)
@@ -52,7 +52,7 @@ func tcpDial(addr string, config *DialConfig) (net.Conn, error) {
 	if err != nil {
 		return nil, common.ContextError(err)
 	}
-	ipAddrs, err := LookupIP(host, config)
+	ipAddrs, err := LookupIP(ctx, host, config)
 	if err != nil {
 		return nil, common.ContextError(err)
 	}
@@ -80,13 +80,14 @@ func tcpDial(addr string, config *DialConfig) (net.Conn, error) {
 	// Iterate over a pseudorandom permutation of the destination
 	// IPs and attempt connections.
 	//
-	// Only continue retrying as long as the initial ConnectTimeout
-	// has not expired. Unlike net.Dial, we do not fractionalize the
-	// timeout, as the ConnectTimeout is generally intended to apply
-	// to a single attempt. So these serial retries are most useful
-	// in cases of immediate failure, such as "no route to host"
+	// Only continue retrying as long as the dial context is not
+	// done. Unlike net.Dial, we do not fractionalize the context
+	// deadline, as the dial is generally intended to apply to a
+	// single attempt. So these serial retries are most useful in
+	// cases of immediate failure, such as "no route to host"
 	// errors when a host resolves to both IPv4 and IPv6 but IPv6
 	// addresses are unreachable.
+	//
 	// Retries at higher levels cover other cases: e.g.,
 	// Controller.remoteServerListFetcher will retry its entire
 	// operation and tcpDial will try a new permutation; or similarly,
@@ -97,17 +98,7 @@ func tcpDial(addr string, config *DialConfig) (net.Conn, error) {
 
 	lastErr := errors.New("unknown error")
 
-	var deadline monotime.Time
-	if config.ConnectTimeout != 0 {
-		deadline = monotime.Now().Add(config.ConnectTimeout)
-	}
-
-	for iteration, index := range permutedIndexes {
-
-		if iteration > 0 && deadline != 0 && monotime.Now().After(deadline) {
-			// lastErr should be set by the previous iteration
-			break
-		}
+	for _, index := range permutedIndexes {
 
 		// Get address type (IPv4 or IPv6)
 
@@ -135,22 +126,20 @@ func tcpDial(addr string, config *DialConfig) (net.Conn, error) {
 
 		// Create a socket and bind to device, when configured to do so
 
-		socketFd, err := syscall.Socket(domain, syscall.SOCK_STREAM, 0)
+		socketFD, err := syscall.Socket(domain, syscall.SOCK_STREAM, 0)
 		if err != nil {
 			lastErr = common.ContextError(err)
 			continue
 		}
 
-		tcpDialSetAdditionalSocketOptions(socketFd)
+		syscall.CloseOnExec(socketFD)
+
+		tcpDialSetAdditionalSocketOptions(socketFD)
 
 		if config.DeviceBinder != nil {
-			// WARNING: this potentially violates the direction to not call into
-			// external components after the Controller may have been stopped.
-			// TODO: rework DeviceBinder as an internal 'service' which can trap
-			// external calls when they should not be made?
-			err = config.DeviceBinder.BindToDevice(socketFd)
+			err = config.DeviceBinder.BindToDevice(socketFD)
 			if err != nil {
-				syscall.Close(socketFd)
+				syscall.Close(socketFD)
 				lastErr = common.ContextError(fmt.Errorf("BindToDevice failed: %s", err))
 				continue
 			}
@@ -158,61 +147,119 @@ func tcpDial(addr string, config *DialConfig) (net.Conn, error) {
 
 		// Connect socket to the server's IP address
 
-		err = syscall.SetNonblock(socketFd, true)
+		err = syscall.SetNonblock(socketFD, true)
 		if err != nil {
-			syscall.Close(socketFd)
+			syscall.Close(socketFD)
 			lastErr = common.ContextError(err)
 			continue
 		}
 
-		err = syscall.Connect(socketFd, sockAddr)
+		err = syscall.Connect(socketFD, sockAddr)
 		if err != nil {
 			if errno, ok := err.(syscall.Errno); !ok || errno != syscall.EINPROGRESS {
-				syscall.Close(socketFd)
+				syscall.Close(socketFD)
 				lastErr = common.ContextError(err)
 				continue
 			}
 		}
 
-		fdset := &goselect.FDSet{}
-		fdset.Set(uintptr(socketFd))
+		// Use a control pipe to interrupt if the dial context is done (timeout or
+		// interrupted) before the TCP connection is established.
 
-		timeout := config.ConnectTimeout
-		if config.ConnectTimeout == 0 {
-			timeout = -1
+		var controlFDs [2]int
+		err = syscall.Pipe(controlFDs[:])
+		if err != nil {
+			syscall.Close(socketFD)
+			lastErr = common.ContextError(err)
+			continue
+
 		}
 
-		err = goselect.Select(socketFd+1, nil, fdset, nil, timeout)
+		for _, controlFD := range controlFDs {
+			syscall.CloseOnExec(controlFD)
+			err = syscall.SetNonblock(controlFD, true)
+			if err != nil {
+				break
+			}
+		}
+
 		if err != nil {
-			syscall.Close(socketFd)
+			syscall.Close(socketFD)
 			lastErr = common.ContextError(err)
 			continue
 		}
 
-		if !fdset.IsSet(uintptr(socketFd)) {
-			syscall.Close(socketFd)
-			lastErr = common.ContextError(errors.New("connect timed out"))
+		resultChannel := make(chan error)
+
+		go func() {
+
+			readSet := goselect.FDSet{}
+			readSet.Set(uintptr(controlFDs[0]))
+			writeSet := goselect.FDSet{}
+			writeSet.Set(uintptr(socketFD))
+
+			max := socketFD
+			if controlFDs[0] > max {
+				max = controlFDs[0]
+			}
+
+			err := goselect.Select(max+1, &readSet, &writeSet, nil, -1)
+
+			if err == nil && !writeSet.IsSet(uintptr(socketFD)) {
+				err = errors.New("interrupted")
+			}
+
+			resultChannel <- err
+		}()
+
+		done := false
+		select {
+		case err = <-resultChannel:
+		case <-ctx.Done():
+			err = ctx.Err()
+			// Interrupt the goroutine
+			// TODO: if this Write fails, abandon the goroutine instead of hanging?
+			var b [1]byte
+			syscall.Write(controlFDs[1], b[:])
+			<-resultChannel
+			done = true
+		}
+
+		syscall.Close(controlFDs[0])
+		syscall.Close(controlFDs[1])
+
+		if err != nil {
+			syscall.Close(socketFD)
+
+			if done {
+				// Skip retry as dial context has timed out of been canceled.
+				return nil, common.ContextError(err)
+			}
+
+			lastErr = common.ContextError(err)
 			continue
 		}
 
-		err = syscall.SetNonblock(socketFd, false)
+		err = syscall.SetNonblock(socketFD, false)
 		if err != nil {
-			syscall.Close(socketFd)
+			syscall.Close(socketFD)
 			lastErr = common.ContextError(err)
 			continue
 		}
 
 		// Convert the socket fd to a net.Conn
+		// This code block is from:
+		// https://github.com/golang/go/issues/6966
 
-		file := os.NewFile(uintptr(socketFd), "")
-		netConn, err := net.FileConn(file) // net.FileConn() dups socketFd
-		file.Close()                       // file.Close() closes socketFd
+		file := os.NewFile(uintptr(socketFD), "")
+		conn, err := net.FileConn(file) // net.FileConn() dups socketFD
+		file.Close()                    // file.Close() closes socketFD
 		if err != nil {
 			lastErr = common.ContextError(err)
 			continue
 		}
 
-		return netConn, nil
+		return &TCPConn{Conn: conn}, nil
 	}
 
 	return nil, lastErr

commit 79b1204d44990ced6c3d42d18e4d4d245c62b918

Fix: Windows syntax errors introduced in 48611af

psiphon/LookupIP_nobind.go
@@ -31,7 +31,7 @@ import (
 
 // LookupIP resolves a hostname. When BindToDevice is not required, it
 // simply uses net.LookupIP.
-func LookupIP(ctx context.Context, host string, _ *DialConfig) ([]net.IP, error) {
+func LookupIP(ctx context.Context, host string, config *DialConfig) ([]net.IP, error) {
 
 	if config.DeviceBinder != nil {
 		return nil, common.ContextError(errors.New("LookupIP with DeviceBinder not supported on this platform"))
@@ -42,7 +42,7 @@ func LookupIP(ctx context.Context, host string, _ *DialConfig) ([]net.IP, error)
 		return nil, common.ContextError(err)
 	}
 
-	ips := make([]netIP, len(addrs))
+	ips := make([]net.IP, len(addrs))
 	for i, addr := range addrs {
 		ips[i] = addr.IP
 	}

psiphon/TCPConn_nobind.go
@@ -38,7 +38,7 @@ func tcpDial(ctx context.Context, addr string, config *DialConfig) (net.Conn, er
 
 	dialer := net.Dialer{}
 
-	conn, err := dialer.DialContext(ctx, "tcp", addr, config.ConnectTimeout)
+	conn, err := dialer.DialContext(ctx, "tcp", addr)
 
 	if err != nil {
 		return nil, common.ContextError(err)