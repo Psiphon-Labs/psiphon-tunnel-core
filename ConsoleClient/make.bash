Psiphon-Labs/psiphon-tunnel-core

commit 615c4e2a4f67b1f88c92ee631e34aa105e52afaf

Fix: reverted necessary parts of Windows build script

ConsoleClient/make.bash
@@ -65,9 +65,15 @@ build_for_windows () {
   fi
 
   if [ -z $1 ] || [ "$1" == "32" ]; then
+    unset PKG_CONFIG_PATH
+    export PKG_CONFIG_PATH=$PKG_CONFIG_PATH_32
 
     echo "...Building windows-i686"
+    echo "....PKG_CONFIG_PATH=$PKG_CONFIG_PATH"
 
+    CGO_CFLAGS="-I $PKG_CONFIG_PATH/include/" \
+    CGO_LDFLAGS="-L $PKG_CONFIG_PATH -L /usr/i686-w64-mingw32/lib/ -lwsock32 -lcrypt32 -lgdi32" \
+    CC=/usr/bin/i686-w64-mingw32-gcc \
     GOOS=windows GOARCH=386 go build -v -x -ldflags "$LDFLAGS" -tags "$WINDOWS_BUILD_TAGS" -o bin/windows/${EXE_BASENAME}-i686.exe
     RETVAL=$?
     echo ".....gox completed, exit code: $?"
@@ -82,9 +88,15 @@ build_for_windows () {
   fi
 
   if [ -z $1 ] || [ "$1" == "64" ]; then
+    unset PKG_CONFIG_PATH
+    export PKG_CONFIG_PATH=$PKG_CONFIG_PATH_64
 
     echo "...Building windows-x86_64"
+    echo "....PKG_CONFIG_PATH=$PKG_CONFIG_PATH"
 
+    CGO_CFLAGS="-I $PKG_CONFIG_PATH/include/" \
+    CGO_LDFLAGS="-L $PKG_CONFIG_PATH -L /usr/x86_64-w64-mingw32/lib/ -lwsock32 -lcrypt32 -lgdi32" \
+    CC=/usr/bin/x86_64-w64-mingw32-gcc \
     GOOS=windows GOARCH=amd64 go build -v -x -ldflags "$LDFLAGS" -tags "$WINDOWS_BUILD_TAGS" -o bin/windows/${EXE_BASENAME}-x86_64.exe
     RETVAL=$?
     if [ $RETVAL != 0 ]; then

commit d9d3fc456e4caf9a8c997530e29b7ade6656fb31

Fix: 615c4e2 broke the Windows build script

ConsoleClient/make.bash
@@ -65,14 +65,9 @@ build_for_windows () {
   fi
 
   if [ -z $1 ] || [ "$1" == "32" ]; then
-    unset PKG_CONFIG_PATH
-    export PKG_CONFIG_PATH=$PKG_CONFIG_PATH_32
-
     echo "...Building windows-i686"
-    echo "....PKG_CONFIG_PATH=$PKG_CONFIG_PATH"
 
-    CGO_CFLAGS="-I $PKG_CONFIG_PATH/include/" \
-    CGO_LDFLAGS="-L $PKG_CONFIG_PATH -L /usr/i686-w64-mingw32/lib/ -lwsock32 -lcrypt32 -lgdi32" \
+    CGO_LDFLAGS="-L /usr/i686-w64-mingw32/lib/ -lwsock32 -lcrypt32 -lgdi32" \
     CC=/usr/bin/i686-w64-mingw32-gcc \
     GOOS=windows GOARCH=386 go build -v -x -ldflags "$LDFLAGS" -tags "$WINDOWS_BUILD_TAGS" -o bin/windows/${EXE_BASENAME}-i686.exe
     RETVAL=$?
@@ -88,14 +83,9 @@ build_for_windows () {
   fi
 
   if [ -z $1 ] || [ "$1" == "64" ]; then
-    unset PKG_CONFIG_PATH
-    export PKG_CONFIG_PATH=$PKG_CONFIG_PATH_64
-
     echo "...Building windows-x86_64"
-    echo "....PKG_CONFIG_PATH=$PKG_CONFIG_PATH"
 
-    CGO_CFLAGS="-I $PKG_CONFIG_PATH/include/" \
-    CGO_LDFLAGS="-L $PKG_CONFIG_PATH -L /usr/x86_64-w64-mingw32/lib/ -lwsock32 -lcrypt32 -lgdi32" \
+    CGO_LDFLAGS="-L /usr/x86_64-w64-mingw32/lib/ -lwsock32 -lcrypt32 -lgdi32" \
     CC=/usr/bin/x86_64-w64-mingw32-gcc \
     GOOS=windows GOARCH=amd64 go build -v -x -ldflags "$LDFLAGS" -tags "$WINDOWS_BUILD_TAGS" -o bin/windows/${EXE_BASENAME}-x86_64.exe
     RETVAL=$?