46e18877678daef4c9af1164d5d794e832f7e2cd
Rod Hynes <rod-hynes@users.noreply.github.com>
2018-10-09 13:59:41 -0400

    Update golang.org/x/crypto subtree
    - "git subtree pull --squash --prefix=psiphon/common/crypto https://go.googlesource.com/crypto master"

--- psiphon/common/crypto/ssh/test/doc.go
+++ psiphon/common/crypto/ssh/test/doc.go
@@ -2,6 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// This package contains integration tests for the
+// Package test contains integration tests for the
 // github.com/Psiphon-Labs/psiphon-tunnel-core/psiphon/common/crypto/ssh package.
 package test // import "github.com/Psiphon-Labs/psiphon-tunnel-core/psiphon/common/crypto/ssh/test"
