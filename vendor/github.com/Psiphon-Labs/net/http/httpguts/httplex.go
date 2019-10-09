c9c7829c07d0f82ca0a8caf40e83069097a41b3d
Rod Hynes <rod-hynes@users.noreply.github.com>
2019-10-09 10:53:33 -0400

    Update vendored Psiphon-Labs/net
    - Import rewrites no longer local patches
    
    - Bug fixes from upstream

--- vendor/github.com/Psiphon-Labs/net/http/httpguts/httplex.go
+++ vendor/github.com/Psiphon-Labs/net/http/httpguts/httplex.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 	"unicode/utf8"
 
-	"golang.org/x/net/idna"
+	"github.com/Psiphon-Labs/net/idna"
 )
 
 var isTokenTable = [127]bool{
--- vendor/github.com/Psiphon-Labs/net/idna/idna10.0.0.go
+++ vendor/github.com/Psiphon-Labs/net/idna/idna10.0.0.go
@@ -4,16 +4,18 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build go1.10
+
 // Package idna implements IDNA2008 using the compatibility processing
 // defined by UTS (Unicode Technical Standard) #46, which defines a standard to
 // deal with the transition from IDNA2003.
 //
 // IDNA2008 (Internationalized Domain Names for Applications), is defined in RFC
 // 5890, RFC 5891, RFC 5892, RFC 5893 and RFC 5894.
-// UTS #46 is defined in http://www.unicode.org/reports/tr46.
-// See http://unicode.org/cldr/utility/idna.jsp for a visualization of the
+// UTS #46 is defined in https://www.unicode.org/reports/tr46.
+// See https://unicode.org/cldr/utility/idna.jsp for a visualization of the
 // differences between these two standards.
-package idna // import "golang.org/x/net/idna"
+package idna // import "github.com/Psiphon-Labs/net/idna"
 
 import (
 	"fmt"
@@ -297,7 +299,7 @@ func (e runeError) Error() string {
 }
 
 // process implements the algorithm described in section 4 of UTS #46,
-// see http://www.unicode.org/reports/tr46.
+// see https://www.unicode.org/reports/tr46.
 func (p *Profile) process(s string, toASCII bool) (string, error) {
 	var err error
 	var isBidi bool
