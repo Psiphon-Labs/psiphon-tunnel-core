Psiphon-Labs/psiphon-tunnel-core

commit dfc850abda5950b3f77b7fa9651694ac4d5dad03

Fix staticcheck issues
- Issues within internal forks remain are untouched

- Fixes web API support for client verification; see 9a0fc18b

- Add additional config parameters to setDialParametersHash,
  which will invalidate existing replay values

psiphon/common/tun/tun_unsupported.go
@@ -46,35 +46,35 @@ func makeDeviceOutboundBuffer(_ int) []byte {
 }
 
 func OpenTunDevice(_ string) (*os.File, string, error) {
-	return nil, "", errors.Trace(unsupportedError)
+	return nil, "", errors.Trace(errUnsupportedor)
 }
 
 func (device *Device) readTunPacket() (int, int, error) {
-	return 0, 0, errors.Trace(unsupportedError)
+	return 0, 0, errors.Trace(errUnsupportedor)
 }
 
 func (device *Device) writeTunPacket(_ []byte) error {
-	return errors.Trace(unsupportedError)
+	return errors.Trace(errUnsupportedor)
 }
 
 func configureNetworkConfigSubprocessCapabilities() error {
-	return errors.Trace(unsupportedError)
+	return errors.Trace(errUnsupportedor)
 }
 
 func resetNATTables(_ *ServerConfig, _ net.IP) error {
-	return errors.Trace(unsupportedError)
+	return errors.Trace(errUnsupportedor)
 }
 
 func configureServerInterface(_ *ServerConfig, _ string) error {
-	return errors.Trace(unsupportedError)
+	return errors.Trace(errUnsupportedor)
 }
 
 func configureClientInterface(_ *ClientConfig, _ string) error {
-	return errors.Trace(unsupportedError)
+	return errors.Trace(errUnsupportedor)
 }
 
 func BindToDevice(_ int, _ string) error {
-	return errors.Trace(unsupportedError)
+	return errors.Trace(errUnsupportedor)
 }
 
 func fixBindToDevice(_ common.Logger, _ bool, _ string) error {
@@ -86,15 +86,15 @@ type NonblockingIO struct {
 }
 
 func NewNonblockingIO(ioFD int) (*NonblockingIO, error) {
-	return nil, errors.Trace(unsupportedError)
+	return nil, errors.Trace(errUnsupportedor)
 }
 
 func (nio *NonblockingIO) Read(p []byte) (int, error) {
-	return 0, errors.Trace(unsupportedError)
+	return 0, errors.Trace(errUnsupportedor)
 }
 
 func (nio *NonblockingIO) Write(p []byte) (int, error) {
-	return 0, errors.Trace(unsupportedError)
+	return 0, errors.Trace(errUnsupportedor)
 }
 
 func (nio *NonblockingIO) IsClosed() bool {
@@ -102,5 +102,5 @@ func (nio *NonblockingIO) IsClosed() bool {
 }
 
 func (nio *NonblockingIO) Close() error {
-	return errors.Trace(unsupportedError)
+	return errors.Trace(errUnsupportedor)
 }

ClientLibrary/PsiphonTunnel.go
@@ -73,8 +73,8 @@ type startResultCode int
 
 const (
 	startResultCodeSuccess    startResultCode = 0
-	startResultCodeTimeout                    = 1
-	startResultCodeOtherError                 = 2
+	startResultCodeTimeout    startResultCode = 1
+	startResultCodeOtherError startResultCode = 2
 )
 
 type startResult struct {
@@ -236,7 +236,7 @@ func PsiphonTunnelStart(cConfigJSON, cEmbeddedServerEntryList *C.char, cParams *
 	// Success
 	managedStartResult = marshalStartResult(startResult{
 		Code:           startResultCodeSuccess,
-		ConnectTimeMS:  int64(time.Now().Sub(startTime) / time.Millisecond),
+		ConnectTimeMS:  int64(time.Since(startTime) / time.Millisecond),
 		HTTPProxyPort:  tunnel.HTTPProxyPort,
 		SOCKSProxyPort: tunnel.SOCKSProxyPort,
 	})

ConsoleClient/main.go
@@ -31,6 +31,7 @@ import (
 	"os/signal"
 	"sort"
 	"sync"
+	"syscall"
 
 	"github.com/Psiphon-Labs/psiphon-tunnel-core/psiphon"
 	"github.com/Psiphon-Labs/psiphon-tunnel-core/psiphon/common"
@@ -290,7 +291,7 @@ func main() {
 	}()
 
 	systemStopSignal := make(chan os.Signal, 1)
-	signal.Notify(systemStopSignal, os.Interrupt, os.Kill)
+	signal.Notify(systemStopSignal, os.Interrupt, syscall.SIGTERM)
 
 	// writeProfilesSignal is nil and non-functional on Windows
 	writeProfilesSignal := makeSIGUSR2Channel()

Server/logging/analysis/analysis.go
@@ -176,7 +176,7 @@ func (a *node) equal(b node) bool {
 					return false
 				}
 				for i := range m {
-					if vNode[i].equal(m[i]) != true {
+					if !vNode[i].equal(m[i]) {
 						return false
 					}
 				}
@@ -393,9 +393,7 @@ func (a *MetricsLogStats) Sort() []MetricsLogModelStats {
 
 func (a *UnknownLogStats) Sort() []UnknownLogModelStats {
 	var s []UnknownLogModelStats
-	for _, v := range a.modelStats {
-		s = append(s, v)
-	}
+	s = append(s, a.modelStats...)
 
 	sort.Slice(s, func(i, j int) bool {
 		return s[j].Count > s[i].Count
@@ -506,7 +504,7 @@ func (l *LogStats) ParseLogLine(log string) error {
 			l.UnknownLogModels.modelStats = append(l.UnknownLogModels.modelStats, UnknownLogModelStats{LogModelStats{1}, *v})
 		}
 	default:
-		return fmt.Errorf("Unexpected model type of %v\n", reflect.TypeOf(v))
+		return fmt.Errorf("unexpected model type of %v", reflect.TypeOf(v))
 	}
 
 	return nil
@@ -523,7 +521,7 @@ func parseLogModel(s string) (LogModel, error) {
 	var m LogFields
 	err := json.Unmarshal([]byte(s), &m)
 	if err != nil {
-		return nil, fmt.Errorf("Failed to parse log line into JSON: %s", err)
+		return nil, fmt.Errorf("failed to parse log line into JSON: %s", err)
 	}
 
 	var l LogModel
@@ -548,7 +546,7 @@ func parseLogModel(s string) (LogModel, error) {
 		case "error":
 			level = LOG_LEVEL_ERROR
 		default:
-			return nil, fmt.Errorf("Unexpected log level: %s\n", m["level"].(string))
+			return nil, fmt.Errorf("unexpected log level: %s", m["level"].(string))
 		}
 
 		var context *MessageLogContext

psiphon/common/accesscontrol/accesscontrol_test.go
@@ -114,7 +114,7 @@ func TestAuthorization(t *testing.T) {
 		t.Fatalf("IssueAuthorization failed: %s", err)
 	}
 
-	verifiedAuth, err = VerifyAuthorization(keyRing, auth)
+	_, err = VerifyAuthorization(keyRing, auth)
 	// TODO: check error message?
 	if err == nil {
 		t.Fatalf("VerifyAuthorization unexpected success")
@@ -129,7 +129,7 @@ func TestAuthorization(t *testing.T) {
 		t.Fatalf("IssueAuthorization failed: %s", err)
 	}
 
-	verifiedAuth, err = VerifyAuthorization(keyRing, auth)
+	_, err = VerifyAuthorization(keyRing, auth)
 	// TODO: check error message?
 	if err == nil {
 		t.Fatalf("VerifyAuthorization unexpected success")
@@ -177,7 +177,7 @@ func TestAuthorization(t *testing.T) {
 
 	encodedSignedAuth := base64.StdEncoding.EncodeToString(marshaledSignedAuth)
 
-	verifiedAuth, err = VerifyAuthorization(keyRing, encodedSignedAuth)
+	_, err = VerifyAuthorization(keyRing, encodedSignedAuth)
 	// TODO: check error message?
 	if err == nil {
 		t.Fatalf("VerifyAuthorization unexpected success")

psiphon/common/authPackage.go
@@ -150,7 +150,7 @@ func ReadAuthenticatedDataPackage(
 		return "", errors.TraceNew("unexpected signing public key type")
 	}
 
-	if 0 != bytes.Compare(
+	if !bytes.Equal(
 		authenticatedDataPackage.SigningPublicKeyDigest,
 		sha256sum(signingPublicKey)) {
 
@@ -294,7 +294,7 @@ func NewAuthenticatedDataPackageReader(
 				return nil, errors.TraceNew("unexpected signing public key type")
 			}
 
-			if 0 != bytes.Compare(jsonSigningPublicKey, sha256sum(signingPublicKey)) {
+			if !bytes.Equal(jsonSigningPublicKey, sha256sum(signingPublicKey)) {
 				return nil, errors.TraceNew("unexpected signing public key digest")
 			}
 
@@ -410,7 +410,7 @@ func (streamer *limitedJSONStreamer) Stream() error {
 				if b == '"' {
 					state = stateJSONSeekingStringValueEnd
 
-					key := string(keyBuffer.Bytes())
+					key := keyBuffer.String()
 
 					// Wrap the main reader in a reader that will read up to the end
 					// of the value and then EOF. The handler is expected to consume

psiphon/common/marionette/marionette_test.go
@@ -75,7 +75,7 @@ func TestMarionette(t *testing.T) {
 					fmt.Printf("Start server conn.Close\n")
 					start := time.Now()
 					conn.Close()
-					fmt.Printf("Done server conn.Close: %s\n", time.Now().Sub(start))
+					fmt.Printf("Done server conn.Close: %s\n", time.Since(start))
 				}()
 				bytesFromClient := 0
 				b := make([]byte, 1024)
@@ -125,7 +125,7 @@ func TestMarionette(t *testing.T) {
 					fmt.Printf("Start client conn.Close\n")
 					start := time.Now()
 					conn.Close()
-					fmt.Printf("Done client conn.Close: %s\n", time.Now().Sub(start))
+					fmt.Printf("Done client conn.Close: %s\n", time.Since(start))
 				}()
 				b := make([]byte, 1024)
 				bytesRead := 0
@@ -167,7 +167,7 @@ func TestMarionette(t *testing.T) {
 	fmt.Printf("Start listener.Close\n")
 	start := time.Now()
 	listener.Close()
-	fmt.Printf("Done listener.Close: %s\n", time.Now().Sub(start))
+	fmt.Printf("Done listener.Close: %s\n", time.Since(start))
 
 	err = testGroup.Wait()
 	if err != nil {

psiphon/common/net_test.go
@@ -77,7 +77,7 @@ func (c *dummyConn) RemoteAddr() net.Addr {
 }
 
 func (c *dummyConn) SetDeadline(t time.Time) error {
-	duration := t.Sub(time.Now())
+	duration := time.Until(t)
 	if c.timeout == nil {
 		c.timeout = time.NewTimer(duration)
 	} else {

commit 4db6a20c820c72367a43cb5ca816c38178a5d596

Fix: typo introduced in dfc850ab

psiphon/common/tun/tun_unsupported.go
@@ -46,35 +46,35 @@ func makeDeviceOutboundBuffer(_ int) []byte {
 }
 
 func OpenTunDevice(_ string) (*os.File, string, error) {
-	return nil, "", errors.Trace(errUnsupportedor)
+	return nil, "", errors.Trace(errUnsupported)
 }
 
 func (device *Device) readTunPacket() (int, int, error) {
-	return 0, 0, errors.Trace(errUnsupportedor)
+	return 0, 0, errors.Trace(errUnsupported)
 }
 
 func (device *Device) writeTunPacket(_ []byte) error {
-	return errors.Trace(errUnsupportedor)
+	return errors.Trace(errUnsupported)
 }
 
 func configureNetworkConfigSubprocessCapabilities() error {
-	return errors.Trace(errUnsupportedor)
+	return errors.Trace(errUnsupported)
 }
 
 func resetNATTables(_ *ServerConfig, _ net.IP) error {
-	return errors.Trace(errUnsupportedor)
+	return errors.Trace(errUnsupported)
 }
 
 func configureServerInterface(_ *ServerConfig, _ string) error {
-	return errors.Trace(errUnsupportedor)
+	return errors.Trace(errUnsupported)
 }
 
 func configureClientInterface(_ *ClientConfig, _ string) error {
-	return errors.Trace(errUnsupportedor)
+	return errors.Trace(errUnsupported)
 }
 
 func BindToDevice(_ int, _ string) error {
-	return errors.Trace(errUnsupportedor)
+	return errors.Trace(errUnsupported)
 }
 
 func fixBindToDevice(_ common.Logger, _ bool, _ string) error {
@@ -86,15 +86,15 @@ type NonblockingIO struct {
 }
 
 func NewNonblockingIO(ioFD int) (*NonblockingIO, error) {
-	return nil, errors.Trace(errUnsupportedor)
+	return nil, errors.Trace(errUnsupported)
 }
 
 func (nio *NonblockingIO) Read(p []byte) (int, error) {
-	return 0, errors.Trace(errUnsupportedor)
+	return 0, errors.Trace(errUnsupported)
 }
 
 func (nio *NonblockingIO) Write(p []byte) (int, error) {
-	return 0, errors.Trace(errUnsupportedor)
+	return 0, errors.Trace(errUnsupported)
 }
 
 func (nio *NonblockingIO) IsClosed() bool {
@@ -102,5 +102,5 @@ func (nio *NonblockingIO) IsClosed() bool {
 }
 
 func (nio *NonblockingIO) Close() error {
-	return errors.Trace(errUnsupportedor)
+	return errors.Trace(errUnsupported)
 }