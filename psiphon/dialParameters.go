42e3f41a149a9309987eb7469d9bf0e4ee193872
Rod Hynes <rod-hynes@users.noreply.github.com>
2023-01-30 15:00:51 -0500

    Meek HTTP header fixes
    - Don't default to including X-Psiphon-Fronting-Address for non-fronted HTTPS;
      only include when configured by tactics
    
    - Always include any selected User-Agent (the metric user_agent already
      reports the selection)

--- psiphon/dialParameters.go
+++ psiphon/dialParameters.go
@@ -934,6 +934,17 @@ func MakeDialParameters(
 	if protocol.TunnelProtocolUsesMeek(dialParams.TunnelProtocol) ||
 		dialParams.ConjureAPIRegistration {
 
+		// For tactics requests, AddPsiphonFrontingHeader is set when set for
+		// the related tunnel protocol. E.g., FRONTED-OSSH-MEEK for
+		// FRONTED-MEEK-TACTICS. AddPsiphonFrontingHeader is not replayed.
+		addPsiphonFrontingHeader := false
+		if dialParams.FrontingProviderID != "" {
+			addPsiphonFrontingHeader = common.Contains(
+				p.LabeledTunnelProtocols(
+					parameters.AddFrontingProviderPsiphonFrontingHeader, dialParams.FrontingProviderID),
+				dialParams.TunnelProtocol)
+		}
+
 		dialParams.meekConfig = &MeekConfig{
 			DiagnosticID:                  serverEntry.GetDiagnosticID(),
 			Parameters:                    config.GetParameters(),
@@ -949,6 +960,7 @@ func MakeDialParameters(
 			RandomizedTLSProfileSeed:      dialParams.RandomizedTLSProfileSeed,
 			UseObfuscatedSessionTickets:   dialParams.TunnelProtocol == protocol.TUNNEL_PROTOCOL_UNFRONTED_MEEK_SESSION_TICKET,
 			SNIServerName:                 dialParams.MeekSNIServerName,
+			AddPsiphonFrontingHeader:      addPsiphonFrontingHeader,
 			VerifyServerName:              dialParams.MeekVerifyServerName,
 			VerifyPins:                    dialParams.MeekVerifyPins,
 			HostHeader:                    dialParams.MeekHostHeader,
@@ -1066,11 +1078,11 @@ func (dialParams *DialParameters) GetTLSVersionForMetrics() string {
 // There are two concerns regarding which dial parameter fields are safe to
 // exchange:
 //
-// - Unlike signed server entries, there's no independent trust anchor
-//   that can certify that the exchange data is valid.
+//   - Unlike signed server entries, there's no independent trust anchor
+//     that can certify that the exchange data is valid.
 //
-// - While users should only perform the exchange with trusted peers,
-//   the user's trust in their peer may be misplaced.
+//   - While users should only perform the exchange with trusted peers,
+//     the user's trust in their peer may be misplaced.
 //
 // This presents the possibility of attack such as the peer sending dial
 // parameters that could be used to trace/monitor/flag the importer; or
