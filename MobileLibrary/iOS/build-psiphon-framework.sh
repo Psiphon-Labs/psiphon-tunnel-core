3f557884c7f180da66b598e4674f8b462b9e387c
Rod Hynes <rod-hynes@users.noreply.github.com>
2023-04-05 13:12:07 -0400

    Use Go 1.19.8

--- MobileLibrary/iOS/build-psiphon-framework.sh
+++ MobileLibrary/iOS/build-psiphon-framework.sh
@@ -17,7 +17,7 @@ set -e -u -x
 if [ -z ${1+x} ]; then BUILD_TAGS=""; else BUILD_TAGS="$1"; fi
 
 # Modify this value as we use newer Go versions.
-GO_VERSION_REQUIRED="1.19.6"
+GO_VERSION_REQUIRED="1.19.8"
 
 # At this time, psiphon-tunnel-core doesn't support modules
 export GO111MODULE=off
