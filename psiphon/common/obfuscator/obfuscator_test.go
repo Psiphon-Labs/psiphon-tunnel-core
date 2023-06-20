908e3937cb59c36a30748890d7eb8dd1ddc359c7
Rod Hynes <rod-hynes@users.noreply.github.com>
2023-06-20 13:34:56 -0400

    Use an ephemeral port in test runs to avoid 'bind: address already in use'

--- psiphon/common/obfuscator/obfuscator_test.go
+++ psiphon/common/obfuscator/obfuscator_test.go
@@ -692,12 +692,12 @@ func obfuscatedSSHConnTestHelper(
 
 	keyword := prng.HexString(32)
 
-	serverAddress := "127.0.0.1:2222"
-
-	listener, err := net.Listen("tcp", serverAddress)
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
 	if err != nil {
 		t.Fatalf("Listen failed: %s", err)
 	}
+
+	serverAddress := listener.Addr().String()
 
 	rsaKey, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
