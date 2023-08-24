5e74a25fc64fee76e6e567a7b33a60e9325f3a91
mirokuratczyk <miro.kuratczyk@gmail.com>
2023-08-24 12:01:48 -0400

    Updated tls-tris vendored module

--- vendor/github.com/Psiphon-Labs/tls-tris/handshake_server.go
+++ vendor/github.com/Psiphon-Labs/tls-tris/handshake_server.go
@@ -165,10 +165,16 @@ func (c *Conn) serverHandshake() error {
 
 				defer conn.Close()
 
+				// Remove any pre-existing deadlines to ensure the passthrough
+				// is not interrupted.
+				_ = conn.SetDeadline(time.Time{})
+
 				passthroughConn, err := net.Dial("tcp", c.config.PassthroughAddress)
 				if err != nil {
 					return
 				}
+				defer passthroughConn.Close()
+
 				_, err = passthroughConn.Write(passthroughReadBuffer)
 				if err != nil {
 					return
