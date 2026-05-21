Psiphon-Labs/psiphon-tunnel-core

commit 5822f0a670dfc292a1e56ea98e37808929d472a5

Fix: limit scope of test fetch mode to specified address

psiphon/controller.go
@@ -2039,7 +2039,13 @@ func (controller *Controller) Dial(
 	if tunnel == nil {
 
 		lightProxyClient, _ := controller.lightProxyClient.Load().(*light.Client)
-		if lightProxyClient != nil {
+		if lightProxyClient != nil &&
+
+			// In test fetch mode, only the test address is routed through
+			// light proxy.
+			(controller.config.LightProxyTestFetchAddress == "" ||
+				remoteAddr == controller.config.LightProxyTestFetchAddress) {
+
 			var lightConn net.Conn
 			lightConn, tunnel, err = controller.dialLightProxyRace(
 				lightProxyClient,

commit 599b54c1bf7f62f6e094f42b60d50345a49451b0

Fix: 5822f0a6 broke fallback to getNextActiveTunnel(0)

psiphon/controller.go
@@ -2012,15 +2012,6 @@ func (controller *Controller) dialLightProxyRace(
 				}, nil, nil
 			}
 
-			// The light dial failed. The final fallback is
-			// Controller.Dial behavior when there is no light
-			// proxy configured and readInactiveThreshold is 0.
-			tunnel := controller.getNextActiveTunnel(0)
-			if tunnel != nil {
-				NoticeWarning(
-					"light proxy dial failed: %v", errors.Trace(result.err))
-				return nil, tunnel, nil
-			}
 			return nil, nil, errors.Trace(result.err)
 
 		case <-ticker.C:
@@ -2091,14 +2082,21 @@ func (controller *Controller) Dial(
 				remoteAddr,
 				downstreamConn,
 				readInactiveThreshold)
-			if err != nil {
-				return nil, errors.Trace(err)
+			if err != nil && controller.runCtx.Err() == nil {
+				NoticeWarning(
+					"light proxy dial failed: %v", errors.Trace(err))
 			}
 
 			if lightConn != nil {
 				return lightConn, nil
 			}
 			// Drop through with tunnel returned from dialLightProxyRace.
+		}
+
+		if tunnel == nil {
+			// The final fallback is the Controller.Dial behavior when there
+			// is no light proxy configured and readInactiveThreshold is 0.
+			tunnel = controller.getNextActiveTunnel(0)
 		}
 
 		if tunnel == nil {