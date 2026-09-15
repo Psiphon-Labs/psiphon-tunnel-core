e90c31b1ae018e979298c63d4cb2ac232b92e1ef
Rod Hynes <rod-hynes@users.noreply.github.com>
2026-09-15 15:34:20 -0400

    Add iPadOS case to normalizeClientPlatform
    - cherry pick 91924821dd9f363fc3e7244d46c956908c682502

--- psiphon/server/api.go
+++ psiphon/server/api.go
@@ -50,6 +50,7 @@ const (
 	CLIENT_PLATFORM_ANDROID = "Android"
 	CLIENT_PLATFORM_WINDOWS = "Windows"
 	CLIENT_PLATFORM_IOS     = "iOS"
+	CLIENT_PLATFORM_IPADOS  = "iPadOS"
 	CLIENT_PLATFORM_OTHER   = "Other"
 
 	SPONSOR_ID_LENGTH = 16
@@ -2154,6 +2155,11 @@ func normalizeClientPlatform(clientPlatform string) string {
 	if strings.Contains(strings.ToLower(clientPlatform), strings.ToLower(CLIENT_PLATFORM_ANDROID)) {
 		return CLIENT_PLATFORM_ANDROID
 	} else if strings.HasPrefix(clientPlatform, CLIENT_PLATFORM_IOS) {
+		return CLIENT_PLATFORM_IOS
+	} else if strings.HasPrefix(clientPlatform, CLIENT_PLATFORM_IPADOS) {
+		// iPadOS is aliased to iOS here, which currently impacts homepage
+		// selection, homepage query parameter substitution, and dest bytes
+		// aggregation buckets.
 		return CLIENT_PLATFORM_IOS
 	} else if strings.HasPrefix(clientPlatform, CLIENT_PLATFORM_WINDOWS) {
 		return CLIENT_PLATFORM_WINDOWS
